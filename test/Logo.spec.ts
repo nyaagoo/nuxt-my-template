07b959e44a98c25e36ddefa9aa51c9d9c87f78bb
nyaagoo <acc.mg47@gmail.com>
2019-11-06 10:37:22 +0900

    [Add] Jest TypeScript Setting

--- test/Logo.spec.ts
+++ test/Logo.spec.ts
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
-import Logo from '@/components/Logo.vue';
+import LogoNuxt from './../components/atoms/Logo.vue';
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo);
+    const wrapper = mount(LogoNuxt);
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
